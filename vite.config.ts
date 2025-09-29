c63daa51eb435169d4c4cb0ef44356ab1d029cd6
Loki3306 <lokesh.gile24@spit.ac.in>
2025-09-29 09:55:26 +0530

    completed basic set-up

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     fs: {
-      allow: ["./admin-portal", "./shared"],
+      allow: ["./admin-portal", "./shared","./"],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "backend/**"],
     },
   },
